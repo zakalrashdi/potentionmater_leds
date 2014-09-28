9bfbe176dd2e91405dda00f79831754504cca776
zakalrashdi <aboyahya99@hotmail.com>
2014-09-28 18:17:43 +0400

    changing in the main int

--- potentionmater_leds.ino
+++ potentionmater_leds.ino
@@ -1,22 +1,33 @@
 
 int leds[] ={1,2,3,4,5,6};
 int x = A0;
-int y;
+float y;
 int z;
 void setup()
 {
     for (int r=1; r < 6; r++)
     {
-      pinMode(leds[r], OUTPUT);
+     pinMode(leds[r], OUTPUT);
     }
 }
 
 void loop()
 {
   z = analogRead(x);
-  y= (z*3/512) ;
-  digitalWrite(y,OUTPUT);
-  delay(500);
-  digitalWrite(y,INPUT);
-  delay(500);
+  y= (z*3/512);
+  
+  for (int s=1; s < 6; s=s+1)
+  {
+    if (s > y)
+      {
+       digitalWrite(s,HIGH);
+       delay(500);
+      }
+    
+    if (s < y )
+    {
+      digitalWrite(s,LOW);
+      delay(500);
+    }
+  }
 }
